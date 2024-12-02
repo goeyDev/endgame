d48271b55cd161eaa48e45c5b94c273c7fb8bae6
goeyDev <chgoey@outlook.com>
2024-12-02 22:55:52 +0800

    modified words.js

--- src/words.js
+++ src/words.js
@@ -43,7 +43,7 @@ export const words = [
   },
   {
     id: 9,
-    name: "komodo Dragon",
+    name: "komododragon",
     imgUrl: "/endgame/komodo-dragon.jpg",
   },
   {
@@ -73,7 +73,7 @@ export const words = [
   },
   {
     id: 15,
-    name: "pygmy hippopotamus",
+    name: "hippopotamus",
     imgUrl: "/endgame/pygmy-hippopotamus.jpg",
   },
   {
