0be4587de84cf10bd23cb8506613281a8cb3a070
goeyDev <chgoey@outlook.com>
2024-12-02 19:04:38 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,218 @@
+import { useState,useEffect } from "react";
+import { languages } from "./languages";
+
+import { getFarewellText,getRandomWord } from "./utils";
+import { clsx } from "clsx";
+import "./App.css";
+import Confetti from "react-confetti";
+// import song from './assets/Rich18.mp3';
+// 134155 functionality
+// 141214
+function AssemblyEndgame() {
+ 
+  // state values
+  const [currentWord, setcurrentWord] = useState(getRandomWord());
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+
+  // Derived values
+  const numGuessesLeft = languages.length - 1;
+  const wrongGuessCount = guessedLetters.filter(
+    (letter) => !currentWord.name.includes(letter)
+  ).length;
+  const isGameWon = currentWord.name
+    .split("")
+    .every((letter) => guessedLetters.includes(letter));
+
+  const isGameLost = wrongGuessCount >= numGuessesLeft;
+  const isGameOver = isGameWon || isGameLost;
+  const lastGuessedLetter = guessedLetters[guessedLetters.length - 1];
+  const isLastGuessedIncorrect =
+    lastGuessedLetter && !currentWord.name.includes(lastGuessedLetter);
+
+  // Static values
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  function addGuessedLetter(letter) {
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+    );
+    console.log(guessedLetters);
+  }
+
+  function addGuessedLetter1(letter) {
+    setGuessedLetters((prevLetters) => {
+      const lettersSet = new Set(prevLetters);
+      lettersSet.add(letter);
+      return Array.from(lettersSet);
+    });
+  }
+
+  // className={`chip ${isLanguageLost ? "lost" : ""}`}
+  const languageElements = languages.map((lang, index) => {
+    const isLanguageLost = index < wrongGuessCount;
+    // console.log("index:",index,"-----isLanguageLost:",isLanguageLost)
+
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    const className = clsx("chip", isLanguageLost && "lost")
+    // const className = clsx("chip");
+    return (
+      <span className={className} key={lang.name} style={styles}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  // display the letter on the screen
+  const letterElements = currentWord.name
+    .split("")
+    .map((letter, index) => {
+      const shouldRevealLetter = isGameLost || guessedLetters.includes(letter)
+      const letterClassName = clsx(
+        isGameLost && !guessedLetters.includes(letter) && "missed-letter"
+      )
+      return(
+        <span key={index} className={letterClassName}>
+          { shouldRevealLetter ? letter.toUpperCase() : ""}
+        </span>
+      )
+      
+    });
+      
+
+  const keyboardElements = alphabet.split("").map((letter) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.name.includes(letter);
+    const isWrong = isGuessed && !currentWord.name.includes(letter);
+    const className = clsx({
+      correct: isCorrect,
+      wrong: isWrong,
+    });
+
+    return (
+      <button
+        key={letter}
+        onClick={() => addGuessedLetter(letter)}
+        className={className}
+        disabled={isGameOver}
+        aria-disabled={guessedLetters.includes(letter)}
+        aria-label={`Letter ${letter}`}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
+
+  const gameStatusClass = clsx("game-status", {
+    won: isGameWon,
+    lost: isGameLost,
+    farewell: !isGameOver && isLastGuessedIncorrect,
+  });
+
+  function renderGameStatus() {
+    if (!isGameOver && isLastGuessedIncorrect) {
+      return (
+        <p className="farewell-message">
+          {getFarewellText(languages[wrongGuessCount - 1].name)}
+        </p>
+      );
+    }
+
+    if (isGameWon) {
+      return (
+        <>
+          <h2>You Win!</h2>
+          <p>Well done!</p>
+        </>
+      );
+    }
+
+    if (isGameLost) {
+      return (
+        <>
+          <h2>Game Over!</h2>
+          <p>You lose! Better start learning Assembly!</p>
+        </>
+      );
+    }
+    return null;
+  }
+
+  function startNewGame(){
+    setcurrentWord(getRandomWord())
+    setGuessedLetters([])
+  }
+
+  // recycle={false}
+  // numberOfPieces={1000}
+
+  useEffect(() => {
+    const music = new Audio('Rich18.mp3');
+    music.loop = true;
+    music.play();
+
+    // Cleanup to stop the music when the component unmounts
+    return () => {
+      music.pause();
+    };
+  }, [isGameWon]);
+  
+  return (
+    <main>
+
+      {
+        isGameWon && <Confetti
+        />
+      }
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world sage
+          from Assembly.
+        </p>
+      </header>
+
+      <section className={gameStatusClass} aria-live="polite" role="status">
+        {renderGameStatus()}
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <div className="images">
+          <img src={currentWord.imgUrl} width={300}/>
+        </div>
+      <section className="sr-only" aria-live="polite" role="status">
+        <p>{currentWord.name.includes(lastGuessedLetter) ? `Correct! The letter ${lastGuessedLetter} is in the word.`
+        :`Sorry,The letter ${lastGuessedLetter} is not in the word.`}
+        You have {numGuessesLeft} attempts left.
+        </p>
+
+
+      <p>Current word:{currentWord.name.split("").map(letter => guessedLetters.includes(letter) ? letter + "." : "blank.").join("")}</p>
+      </section>
+      <section className="keyboard">{keyboardElements}</section>
+      {isGameOver && <button className="new-game" onClick={startNewGame}>New Game</button>}
+    </main>
+  );
+}
+
+export default AssemblyEndgame;
+
+{
+  /* <section className="game-status">
+  {isGameOver ?(
+    isGameWon ?(
+      <>
+      <h2>You Win!</h2>
+      <p>Well done!</p></>
+    ):(
+      <>
+      <h2>Game Over!</h2>
+      <p>You lose! Better start learning Assembly!</p>
+      </>
+    )
+  ) :null}
+</section>  */
+}
---
+++ src/words.js
@@ -0,0 +1,204 @@
+// 20 most famous animal noun in singapore zoo
+
+export const words = [
+  {
+    id: 1,
+    name: "elephant",
+    imgUrl: "/elephant.webp",
+  },
+  {
+    id: 2,
+    name: "lion",
+    imgUrl: "/lion.webp",
+  },
+  {
+    id: 3,
+    name: "panda",
+    imgUrl: "/panda.webp",
+  },
+  {
+    id: 4,
+    name: "giraffe",
+    imgUrl: "/giraffe.webp",
+  },
+  {
+    id: 5,
+    name: "leopard",
+    imgUrl: "/Leopard.jpg",
+  },
+  {
+    id: 6,
+    name: "tiger",
+    imgUrl: "/tiger.jpg",
+  },
+  {
+    id: 7,
+    name: "zebra",
+    imgUrl: "/zebra.jpg",
+  },
+  {
+    id: 8,
+    name: "kangaroo",
+    imgUrl: "/kangaroo.jpg",
+  },
+  {
+    id: 9,
+    name: "komodo Dragon",
+    imgUrl: "/komodo-dragon.jpg",
+  },
+  {
+    id: 10,
+    name: "cheetah",
+    imgUrl: "/cheetah.jpg",
+  },
+  {
+    id: 11,
+    name: "flamingo",
+    imgUrl: "/flamingo.jpg",
+  },
+  {
+    id: 12,
+    name: "rhino",
+    imgUrl: "/rhino.jpg",
+  },
+  {
+    id: 13,
+    name: "chimpanzee",
+    imgUrl: "/chimpanzee.jpg",
+  },
+  {
+    id: 14,
+    name: "otter",
+    imgUrl: "/otter.jpg",
+  },
+  {
+    id: 15,
+    name: "pygmy hippopotamus",
+    imgUrl: "/pygmy-hippopotamus.jpg",
+  },
+  {
+    id: 16,
+    name: "sloth",
+    imgUrl: "/sloth.jpg",
+  },
+  {
+    id: 17,
+    name: "leopard",
+    imgUrl: "/leopard.jpg",
+  },
+  {
+    id: 18,
+    name: "penguin",
+    imgUrl: "/penguin.jpg",
+  },
+  {
+    id: 19,
+    name: "crocodile",
+    imgUrl: "/crocodile.jpg",
+  },
+  {
+    id: 20,
+    name: "koala",
+    imgUrl: "/koala.jpg",
+  },
+//   {
+//     id: 21,
+//     name: "Binturong",
+//     imgUrl: "/binturong.png",
+//   },
+//   {
+//     id: 22,
+//     name: "Mongoose",
+//     imgUrl: "/mongoose.png",
+//   },
+//   {
+//     id: 23,
+//     name: "Clouded Leopard",
+//     imgUrl: "/clouded-leopard.png",
+//   },
+//   {
+//     id: 24,
+//     name: "Macaw",
+//     imgUrl: "/macaw.png",
+//   },
+//   {
+//     id: 25,
+//     name: "Red Panda",
+//     imgUrl: "/red-panda.png",
+//   },
+//   {
+//     id: 26,
+//     name: "Water Buffalo",
+//     imgUrl: "/water-buffalo.png",
+//   },
+//   {
+//     id: 27,
+//     name: "Swan",
+//     imgUrl: "/swan.png",
+//   },
+//   {
+//     id: 28,
+//     name: "Malayan Tiger",
+//     imgUrl: "/malayan-tiger.png",
+//   },
+//   {
+//     id: 29,
+//     name: "Hornbill",
+//     imgUrl: "/hornbill.png",
+//   },
+//   {
+//     id: 30,
+//     name: "Warthog",
+//     imgUrl: "/warthog.png",
+//   },
+//   {
+//     id: 31,
+//     name: "Bengal Tiger",
+//     imgUrl: "/bengal-tiger.png",
+//   },
+//   {
+//     id: 32,
+//     name: "Okapi",
+//     imgUrl: "/okapi.png",
+//   },
+//   {
+//     id: 33,
+//     name: "Kangaroo Rat",
+//     imgUrl: "/kangaroo-rat.png",
+//   },
+//   {
+//     id: 34,
+//     name: "Arctic Fox",
+//     imgUrl: "/arctic-fox.png",
+//   },
+//   {
+//     id: 35,
+//     name: "Manta Ray",
+//     imgUrl: "/manta-ray.png",
+//   },
+//   {
+//     id: 36,
+//     name: "Gibbon",
+//     imgUrl: "/gibbon.png",
+//   },
+//   {
+//     id: 37,
+//     name: "Jaguar",
+//     imgUrl: "/aguar.png",
+//   },
+//   {
+//     id: 38,
+//     name: "Leopard Tortoise",
+//     imgUrl: "/leopard-tortoise.png",
+//   },
+//   {
+//     id: 39,
+//     name: "Giant Tortoise",
+//     imgUrl: "/giant-tortoise.png",
+//   },
+//   {
+//     id: 40,
+//     name: "Kingfisher",
+//     imgUrl: "/kingfisher.png",
+//   },
+];
